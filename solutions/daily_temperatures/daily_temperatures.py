debe80c61cf8369fea4886f2e2f3d0788b06d564
PATEL MANTHANKUMAR LALITKUMAR <107125884+manthan161203@users.noreply.github.com>
2025-11-27 15:05:47 +0530

    Delete solutions directory

--- solutions/daily_temperatures/daily_temperatures.py
+++
@@ -1,3 +0,0 @@
-# Solution - PYTHON
-
-class Solution:\n    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:\n        stack = []\n        n = len(temperatures)\n        answer = [0] * n\n        for i in range(n):\n            while stack and temperatures[i] > temperatures[stack[-1]]:\n                idx = stack.pop()\n                answer[idx] = i - idx\n            stack.append(i)\n        return answer
